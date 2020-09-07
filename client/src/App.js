05bc861f17145cc9e03cfaca0291f8b82decb01c
Sofiya Lazarova <sofiya@sofiya.ca>
2020-09-07 18:12:00 +0200

    Update list of movies when search term updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,28 @@ import React, { useState, useEffect } from "react";
 import ApiClient from "./Services/ApiClient";
 import MovieList from "./Containers/MovieList";
 import "./App.scss";
+import SearchBox from "./Components/SearchForm/SearchForm";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [displayError, setDisplayError] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  // List new movies when the search term is updated.
+  // Show an alert if no movies were found.
   useEffect(() => {
-    ApiClient.getMovies("batman").then((movieResults) => {
+    ApiClient.getMovies(searchTerm).then((movieResults) => {
       if (movieResults.Response === "True") {
         setMovies(movieResults.Search);
       } else {
-        setDisplayError(true);
+        alert("No movies were found, try a different search term.");
       }
     });
-  }, []);
+  }, [searchTerm]);
 
   return (
     <div className="App">
-      <h3>Edited App</h3>
+      <h1>Welcome to Movie Search</h1>
+      <SearchBox setSearchTerm={setSearchTerm}></SearchBox>
       <MovieList movies={movies} />
     </div>
   );
