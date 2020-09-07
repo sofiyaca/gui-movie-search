cd41bcdfc3bc06fb6fe944296630ea93883754aa
Sofiya Lazarova <sofiya@sofiya.ca>
2020-09-07 16:35:26 +0200

    Display a list of movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ApiClient from "./Services/ApiClient";
+import MovieList from "./Containers/MovieList";
 import "./App.scss";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [displayError, setDisplayError] = useState(false);
+
+  useEffect(() => {
+    ApiClient.getMovies("batman").then((movieResults) => {
+      if (movieResults.Response === "True") {
+        setMovies(movieResults.Search);
+      } else {
+        setDisplayError(true);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <h3>Edited App</h3>
+      <MovieList movies={movies} />
     </div>
   );
 }
